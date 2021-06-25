87924b09961415c1ed49b77f3d01324d17120f8d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-06-25 17:06:05 +0300

    deleted many things, added bootstrap cdn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react"
-
+import WelcomePage from "./pages/index"
 
 function App() {
   return (
     <div className="App">
-      yoooowww
+     <WelcomePage />
+     <button className="btn btn-danger">dang</button>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,24 @@
 import React from "react"
-import Layout from "./Layout"
+// import Layout from "./Layout"
 import {indexContainer} from "../styles/layout.module.css"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 export default function Home() {
   return (
-  <Layout>
+  // <Layout>
 <div className={indexContainer}>
 {/* <div>Hello world bruh</div> */}
 
 <img src="/welcome.jpg" style={{maxWidth:"100%"}, {maxHeight:"55vh"}} />
-<button type="button" style={{marginTop:"3em"}} class="btn btn-danger btn-lg">
-  <Link style={ {color:"black"}} to="/Shop">
+<button type="button" style={{marginTop:"3em"}} className="btn btn-danger btn-lg">
+  {/* <Link style={ {color:"black"}} to="/Shop"> */}
   <del>SHOP</del> CONSUME NOW
-  </Link>
+  {/* </Link> */}
   </button>
 
 </div>
 
 
-  </Layout>
+  // </Layout>
   )
 }
