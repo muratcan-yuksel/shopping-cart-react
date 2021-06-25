cd59059dac0382bf069a53101195eb7332ed72e5
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-06-25 20:00:25 +0300

    added anarchism routing

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
 import WelcomePage from "./pages/index"
 import Shop from "./pages/Shop"
+import AnarchismPage from "./pages/AnarchismPage"
+
 
 const Routes = () => {
   return (
@@ -11,6 +13,8 @@ const Routes = () => {
         <Route exact path="/" component={App} />
         <Route exact path="/welcome" component={WelcomePage} />
         <Route exact path="/Shop" component={Shop} />
+        <Route exact path="/AnarchismPage" component={AnarchismPage} />
+
 
       </Switch>
     </BrowserRouter>
