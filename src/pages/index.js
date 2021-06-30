54f04a8bef1fdd8372504d3a8c1aea994661b8a5
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-07-01 01:00:52 +0300

    solved the same issue with consume button

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
 <button type="button" style={{marginTop:"3em"}} className="btn btn-danger btn-lg">
 
-  <Link style={ {color:"black"}} to="/Shop">
+  <Link className="consumeBtn" to="/Shop">
   <del>SHOP</del> CONSUME NOW
   </Link>
   
