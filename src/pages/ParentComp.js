2abaf5d7fdc430d1d69352c3693a2174e3c80462
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-06-30 16:40:17 +0300

    adding the parentcomp file which will contain the pages to share state within

---
+++ src/pages/ParentComp.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import AnarchismPage from './AnarchismPage'
+import HobgoblinPage from "./HobgoblinPage"
+import MadMarxBlackPage from "./MadMarxBlackPage"
+import MadMarxPage from "./MadMarxPage"
+import MarxLemonPage from "./MarxLemonPage"
+import PhilosophyClubPage from "./PhilosophyClubPage"
+import RadicalFreedomPage from "./RadicalFreedomPage"
+import RevolutionPage from "./RevolutionPage"
+import UpsideHegelPage from "./UpsideHegelPage"
+import WrongHegelPage from "./WrongHegelPage"
+
+export default function ParentComp() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,15 @@ export default function Home() {
   return (
   <Layout>
 <div className="indexContainer">
-{/* <div>Hello world bruh</div> */}
 
 <img src={WelcomeImg} style={{maxWidth:"100%"}, {maxHeight:"55vh"}} />
+
 <button type="button" style={{marginTop:"3em"}} className="btn btn-danger btn-lg">
+
   <Link style={ {color:"black"}} to="/Shop">
   <del>SHOP</del> CONSUME NOW
   </Link>
+  
   </button>
 
 </div>
