fa8ac9420ed35a69555661ceb722016b074a01ca
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-07-03 21:51:46 +0300

    centered the checkout button

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -1,7 +1,8 @@
 import React, {useState, useContext, useEffect} from 'react'
 import { ItemContext } from './ItemProvider'
 import Layout from "./Layout"
-import uniqid from "uniqid";
+import "../styles/layout.css"
+
 
 export default function CheckOut() {
 
@@ -44,33 +45,12 @@ const mapped = items.map((item=>{
       <Layout>
         <div>
         { mapped  }
+        <div className="btns">
         <button className="btn btn-danger">Finalize order</button>
-
+        </div>
         </div>
         </Layout>
     )
 }
 
 
-   {/* <div className="shopContainer">
-    <div 
-    className=" col-8 col-md-4 col-lg-4  text-center" 
-    >
-  <div className="card h-100 mx-auto w-100 ">          
-      <img      
-     src={item.img}
-    className="card-img-top h-100 "
-        alt={item.title}
-      ></img>      
-      <div className="card-body">
-        <p className="card-text">{item.title}</p>
-        <p className="card-text">{item.sign} {item.price}</p>
-        <div class= "amountBtns">
-        <button style={{height:"3em"}} type="button" class="btn btn-dark">-</button>
-        <p style={{fontSize:"2rem"}, {margin:"1rem"}} className="card-text">{item.amount}</p>
-        <button  style={{height:"3em"}} type="button" class="btn btn-dark">+</button>            </div>
-      </div>
-    </div>
-    </div>
-    <button  style={{marginTop:"1rem"}} className="btn btn-danger">Add to cart</button>
-    </div> */}
