d91fcd03a309d5c032d54bc926160227a27d127e
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-07-03 17:41:49 +0300

    need to work on it, can't display in checkout

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -1,5 +1,6 @@
-import React, {useState, useContext} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
 import { ItemContext } from './ItemProvider'
+import Layout from "./Layout"
 
 export default function CheckOut() {
 
@@ -7,9 +8,50 @@ export default function CheckOut() {
   
 console.log(items)
 
+useEffect(() => {
+  const mappedItems= items.map(item=>{
+    <div>
+    console.log(item.title, item.price)
+    <p>{item.title}</p>
+    </div>
+  })}, [items]);
+
+const mappedItems= items.map(item=>{
+  <div>
+  console.log(item.title, item.price)
+  <p>{item.title}</p>
+  </div>
+})
+
     return (
+      <Layout>
         <div>
-yows            
+        {mappedItems}          
         </div>
+        </Layout>
     )
 }
+
+
+   {/* <div className="shopContainer">
+    <div 
+    className=" col-8 col-md-4 col-lg-4  text-center" 
+    >
+  <div className="card h-100 mx-auto w-100 ">          
+      <img      
+     src={item.img}
+    className="card-img-top h-100 "
+        alt={item.title}
+      ></img>      
+      <div className="card-body">
+        <p className="card-text">{item.title}</p>
+        <p className="card-text">{item.sign} {item.price}</p>
+        <div class= "amountBtns">
+        <button style={{height:"3em"}} type="button" class="btn btn-dark">-</button>
+        <p style={{fontSize:"2rem"}, {margin:"1rem"}} className="card-text">{item.amount}</p>
+        <button  style={{height:"3em"}} type="button" class="btn btn-dark">+</button>            </div>
+      </div>
+    </div>
+    </div>
+    <button  style={{marginTop:"1rem"}} className="btn btn-danger">Add to cart</button>
+    </div> */}
