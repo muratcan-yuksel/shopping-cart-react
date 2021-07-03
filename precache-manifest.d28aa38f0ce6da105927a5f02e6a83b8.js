ef6f18d1a7550a0b831a2672807dc9efe115eeea
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-07-04 01:05:07 +0300

    Updates

---
+++ precache-manifest.d28aa38f0ce6da105927a5f02e6a83b8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "b80160333cef509e379e",
+    "url": "/shopping-cart-react/static/css/main.24144fa5.chunk.css"
+  },
+  {
+    "revision": "b80160333cef509e379e",
+    "url": "/shopping-cart-react/static/js/main.fd60048a.chunk.js"
+  },
+  {
+    "revision": "218f8eb1b92a1be609cd",
+    "url": "/shopping-cart-react/static/js/runtime~main.36ec2802.js"
+  },
+  {
+    "revision": "e09ec0d12ac68626846e",
+    "url": "/shopping-cart-react/static/css/2.6054450b.chunk.css"
+  },
+  {
+    "revision": "e09ec0d12ac68626846e",
+    "url": "/shopping-cart-react/static/js/2.b0f45a15.chunk.js"
+  },
+  {
+    "revision": "f0e62ee949d00a4bf2055acd95f5230f",
+    "url": "/shopping-cart-react/static/media/hegel-was-wrong.f0e62ee9.png"
+  },
+  {
+    "revision": "a9cb7ef5c00fabfa169cad767de64daf",
+    "url": "/shopping-cart-react/static/media/anarchism.a9cb7ef5.png"
+  },
+  {
+    "revision": "301c118b5dc5673eaa6a7ece1ae34163",
+    "url": "/shopping-cart-react/static/media/hobgoblin.301c118b.png"
+  },
+  {
+    "revision": "8a1ac3637041654635e5007b963324d8",
+    "url": "/shopping-cart-react/static/media/mad-marx.8a1ac363.png"
+  },
+  {
+    "revision": "582a67feb99596906658b9ac62f43248",
+    "url": "/shopping-cart-react/static/media/mad-marx-black.582a67fe.png"
+  },
+  {
+    "revision": "e4e7c2570275d17b31200387cf0d4245",
+    "url": "/shopping-cart-react/static/media/marx-lemon.e4e7c257.png"
+  },
+  {
+    "revision": "e506b6a71287353f3bbd1b8371229828",
+    "url": "/shopping-cart-react/static/media/philo-club.e506b6a7.png"
+  },
+  {
+    "revision": "c8a183347ad8b68cb2dc1f79d66acacc",
+    "url": "/shopping-cart-react/static/media/radical-freedom.c8a18334.png"
+  },
+  {
+    "revision": "c0c5a888d1f33e5eab3922b4f96f9c4d",
+    "url": "/shopping-cart-react/static/media/revolution.c0c5a888.png"
+  },
+  {
+    "revision": "1f4f1481760fa56d9332a72c8a7dee53",
+    "url": "/shopping-cart-react/static/media/upside-hegel.1f4f1481.png"
+  },
+  {
+    "revision": "3dcaa857179a507a55858c075ac01452",
+    "url": "/shopping-cart-react/static/media/welcome.3dcaa857.jpg"
+  },
+  {
+    "revision": "e8dbcc48f12bbcf12128d98e09804192",
+    "url": "/shopping-cart-react/index.html"
+  }
+];
